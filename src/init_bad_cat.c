e98844ad0a41e8798c88d2781282e6306cf30025
xinwang <xinwang@students.42.fr>
2020-08-29 12:21:10 +0200

    Fix leaks on bad_cat

--- src/init_bad_cat.c
+++ src/init_bad_cat.c
@@ -17,17 +17,25 @@ void	init_bad_cat(t_map *rvar)
 	int		i;
 	char	*s;
 	char	*s1;
+	char	*n_tmp;
 
+	s = NULL;
+	s1 = NULL;
+	n_tmp = NULL;
 	i = -1;
 	rvar->nb_bad_cat = 0;
 	if (!(rvar->bad_cat = (t_bad_cat *)malloc(sizeof(t_bad_cat) * 31)))
 		free_program_var(rvar, "Malloc failed of bad_cat\n", NULL);
 	while (++i <= 30)
 	{
-		s = ft_strjoin("./textures/enemies/badcat/bad_cat_", ft_itoa(i));
+
+		n_tmp = ft_itoa(i);
+		s = ft_strjoin("./textures/enemies/badcat/bad_cat_", n_tmp);
+		free_str(n_tmp);
 		s1 = ft_strjoin(s, ".xpm");
 		free_str(s);
 		rvar->bad_cat[i].path = ft_strnew(ft_strlen(s1) + 1);
 		rvar->bad_cat[i].path = ft_strcat(rvar->bad_cat[i].path, s1);
+		free_str(s1);
 	}
 }
