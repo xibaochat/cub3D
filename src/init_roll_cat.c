979c4b7fac2b4f59e471d49c8087225aff305244
xinwang <xinwang@students.42.fr>
2020-08-29 12:24:47 +0200

    Fix leaks on roll_cat

--- src/init_roll_cat.c
+++ src/init_roll_cat.c
@@ -17,17 +17,24 @@ void	init_roll_cat(t_map *rvar)
 	int		i;
 	char	*s;
 	char	*s1;
+	char	*n_tmp;
 
+	s = NULL;
+	s1 = NULL;
+	n_tmp = NULL;
 	i = -1;
 	rvar->nb_roll_cat = 0;
 	rvar->roll_cat = (t_roll_cat *)malloc(sizeof(t_roll_cat) * 16);
 	rvar->roll_cat->nb_tex = 0;
 	while (++i <= 15)
 	{
-		s = ft_strjoin("./textures/texture_", ft_itoa(i));
+		n_tmp = ft_itoa(i);
+		s = ft_strjoin("./textures/texture_", n_tmp);
+		free_str(n_tmp);
 		s1 = ft_strjoin(s, ".xpm");
 		free_str(s);
 		rvar->roll_cat[i].path = ft_strnew(ft_strlen(s1) + 1);
 		rvar->roll_cat[i].path = ft_strcat(rvar->roll_cat[i].path, s1);
+		free_str(s1);
 	}
 }
